46761eea38c90bb598cfebce3ea17f53aae014d6
khasanov2oo9 <fazliddinhasanov34@gmail.com>
2025-05-25 17:42:37 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let string = prompt("String typedagi ma'lumot kiriting !");
+let number  = +prompt("Number typedagi ma'lumot kiriting !");
+let boolean = !prompt("Boolean typedagi ma'lumot kiriting !");
+
+let request = confirm("Hamma ma'lumotlarni to'g'ri kiritganingizga ishonchingiz komilmi ?");
+
+
+let st = alert("Siz kiritgan " + string + " so'zi " + typeof + " typega tegishli\n")
+num = alert("Siz kiritgan " + number + " so'zi " + typeof + " typega tegishli\n")
+bool = alert("Siz kiritgan " + boolean + " so'zi " + typeof + " typega tegishli");
